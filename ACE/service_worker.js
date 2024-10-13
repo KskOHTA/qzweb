c762fb88213af67d2f449328e436c9c43c1b902b
KskOHTA <ksk_ohta@ybb.ne.jp>
2024-10-13 19:20:52 +0900

    Add files via upload

--- ACE/service_worker.js
+++ ACE/service_worker.js
@@ -25,7 +25,7 @@ self.addEventListener('install', function(event) {
         caches
             .open(CACHE_NAME)
             .then(function(cache) {
-	        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+	        return cache.addAll(urlsToCache);
             })
     );
 });
